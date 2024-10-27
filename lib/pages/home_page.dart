87f7669ef6889cb18d4629c7df2aa4d0077b38bc
yurigrams <yurigrams@gmail.com>
2024-10-27 16:59:32 -0300

    retirando o grafico da HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,26 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
 class HomePage extends StatelessWidget {
-  final List<double> monthlyExpenses = [
-    500,
-    700,
-    600,
-    400,
-    800,
-    750,
-    670,
-    900,
-    850,
-    720,
-    660,
-    780
-  ];
 
   @override
   Widget build(BuildContext context) {
-    double maxY = monthlyExpenses.isNotEmpty
-        ? monthlyExpenses.reduce((a, b) => a > b ? a : b) + 100
-        : 1000;
 
     return BasePage(
       titles: 'Gráfico de Gastos',
@@ -31,35 +14,12 @@ class HomePage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 'Gastos Mensais',
                 style: TextStyle(
                   fontSize: 30,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 20),
-                height: 300,
-                child: BarChart(
-                  BarChartData(
-                    alignment: BarChartAlignment.spaceAround,
-                    maxY: maxY.isFinite ? maxY : 1000,
-                    barGroups: List.generate(
-                      monthlyExpenses.length,
-                          (index) => BarChartGroupData(
-                        x: index,
-                        barRods: [
-                          BarChartRodData(
-                            toY: monthlyExpenses[index].isFinite
-                                ? monthlyExpenses[index]
-                                : 0,
-                            color: Colors.blue,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
                 ),
               ),
               Text(
